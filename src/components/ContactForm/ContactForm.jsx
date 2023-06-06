0444b2272619aef590dc5903bd26467ba3fbae8a
braunalex74 <braunalex74@googlemail.com>
2023-06-06 21:01:37 +0300

    delete_constructor

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,22 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { Form } from './ContactForm.styled';
 
-class ContactForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      number: '',
-    };
-  }
+const ContactForm = ({ onAddContact, contacts }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-
-    const { name, number } = this.state;
-    const { contacts, onAddContact } = this.props;
 
     const isDuplicate = contacts.some(
       contact =>
@@ -28,54 +20,51 @@ class ContactForm extends React.Component {
       alert(`${name} is already in contacts.`);
     } else {
       onAddContact(name, number);
-      this.setState({ name: '', number: '' });
+      setName('');
+      setNumber('');
     }
   };
 
-  handleNameChange = event => {
-    this.setState({ name: event.target.value });
+  const handleNameChange = event => {
+    setName(event.target.value);
   };
 
-  handleNumberChange = event => {
-    this.setState({ number: event.target.value });
+  const handleNumberChange = event => {
+    setNumber(event.target.value);
   };
 
-  render() {
-    const { name, number } = this.state;
-
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces."
-            required
-            value={name}
-            onChange={this.handleNameChange}
-          />
-        </label>
-        <br />
-        <label>
-          Number:
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={number}
-            onChange={this.handleNumberChange}
-          />
-        </label>
-        <br />
-        <button type="submit">Add Contact</button>
-      </Form>
-    );
-  }
-}
+  return (
+    <Form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces."
+          required
+          value={name}
+          onChange={handleNameChange}
+        />
+      </label>
+      <br />
+      <label>
+        Number:
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={handleNumberChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Add Contact</button>
+    </Form>
+  );
+};
 
 ContactForm.propTypes = {
   onAddContact: PropTypes.func.isRequired,
